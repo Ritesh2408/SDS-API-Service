e9ecaad68a673259d451b6a074bccb1291699bfd
Ritesh Narendra Chaudhari <76548605+Ritesh2408@users.noreply.github.com>
2021-09-05 21:50:18 +0530

    Added with little frontend

--- README.md
+++
@@ -1,2 +0,0 @@
-# SDS-API-Service
-An API which can take input from users and can perform CRUD operations with the user data.
--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 import os
 from flask import Flask,  request, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api,Resource
-from secure_check import authenticate,identity
 from flask_jwt import JWT ,jwt_required
 from flask_migrate import Migrate
 
-
 app = Flask(__name__)
-
 #configuring and setting up database
 app.config['SECRET_KEY'] = 'mysecretkey'
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///C:/Users/Ritesh Chaudhari/Desktop/SDS_API/sds.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 db = SQLAlchemy(app)
 Migrate(app,db)
-jwt = JWT(app, authenticate, identity)
+#jwt = JWT(app, authenticate, identity)
 api = Api(app)
 
-
-# Creating models
+#CREATING models
 
 class SDS(db.Model):
+
+    id = db.Column(db.Integer,primary_key=True, nullable=True)
     name = db.Column(db.String(80))
-    mis = db.Column(db.Integer(), primary_key=True)
+    mis = db.Column(db.Integer())
     email = db.Column(db.String())
 
 
@@ -40,7 +40,58 @@ class SDS(db.Model):
     def __str__(self):
         return f" Name of registered student: {self.name}\nMIS: {self.mis}\nEmail: {self.email}"
 
-#Resources
+#Setting up routes
+@app.route('/')
+def Index():
+    students = SDS.query.all()
+
+    return render_template("index.html", students = students)
+
+@app.route('/insert', methods = ['POST'])
+def insert():
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        email = request.form['email']
+        mis = request.form['mis']
+
+
+        my_data = SDS(name, email, mis)
+        db.session.add(my_data)
+        db.session.commit()
+
+        flash("Student Inserted Successfully")
+
+        return redirect(url_for('Index'))
+
+@app.route('/update', methods = ['GET', 'POST'])
+def update():
+
+    if request.method == 'POST':
+        my_data = SDS.query.get(request.form.get('id'))
+
+        my_data.name = request.form['name']
+        my_data.email = request.form['email']
+        my_data.mis = request.form['mis']
+
+        db.session.commit()
+        flash("Student Updated Successfully")
+
+        return redirect(url_for('Index'))
+
+@app.route('/delete/<id>/', methods = ['GET', 'POST'])
+def delete(id):
+    my_data = SDS.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Student Deleted Successfully")
+
+    return redirect(url_for('Index'))
+
+
+#RESIURCESSS
+
 class SDSResource(Resource):
     def get(self,name,mis,email):
 
@@ -71,8 +122,6 @@ class SDSResource(Resource):
         return {'note':'delete successful'}
 
 
-
-
 class AllStudents(Resource):
 
     #@jwt_required()
@@ -84,8 +133,9 @@ class AllStudents(Resource):
         return [obj.json() for obj in students]
 
 
+
 api.add_resource(SDSResource, '/user/<string:name>/<int:mis>/<string:email>')
 api.add_resource(AllStudents,'/students')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
